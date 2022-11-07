6a1149194c8fecded22db69d2a694dc85a10b6fd
Anthony Dombrowski <akdflyinvee73@gmail.com>
2022-11-06 19:47:17 -0600

    removed some logging

--- passwordlesser/src/App.tsx
+++ passwordlesser/src/App.tsx
@@ -29,10 +29,6 @@ const rndPositionFromLeftEdge = () =>
 // };
 // const colWidth = colWidthNumber(NUMBER_OF_DAVINCIS).toString() + "vw";
 
-console.log();
-console.log(rndPositionFromLeftEdge());
-console.log();
-
 const mappingDVs = (dvContainers: number[]) => {
   return (
     <div
