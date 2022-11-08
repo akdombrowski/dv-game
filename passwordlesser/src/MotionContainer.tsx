cd3a186c44a626e8bf7eadb14e88e1b7ce0f6d51
Anthony Dombrowski <akdflyinvee73@gmail.com>
2022-11-07 19:06:12 -0600

    deleted some comments

--- passwordlesser/src/MotionContainer.tsx
+++ passwordlesser/src/MotionContainer.tsx
@@ -1,29 +1,5 @@
-import React, { SyntheticEvent, useEffect } from "react";
-import PropTypes from "prop-types";
+import { SyntheticEvent } from "react";
 import { motion } from "framer-motion";
-import { isNamedTupleMember } from "typescript";
-
-// example from mdn
-// async function postData(url = "", data = {}) {
-//   // Default options are marked with *
-//   const response = await fetch(url, {
-//     method: "POST", // *GET, POST, PUT, DELETE, etc.
-//     mode: "cors", // no-cors, *cors, same-origin
-//     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-//     credentials: "same-origin", // include, *same-origin, omit
-//     headers: {
-//       "Content-Type": "application/json",
-//       // 'Content-Type': 'application/x-www-form-urlencoded',
-//     },
-//     redirect: "follow", // manual, *follow, error
-//     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-//     body: JSON.stringify(data), // body data type must match "Content-Type" header
-//   });
-//   return response.json(); // parses JSON response into native JavaScript objects
-// }
-// postData("https://example.com/answer", { answer: 42 }).then((data) => {
-//   console.log(data); // JSON data parsed by `data.json()` call
-// });
 
 const CONTINUE_BTN_VAL = "found-me";
 const XSRF_TOKEN_COOKIE_NAME = "XSRF-TOKEN";
@@ -64,27 +40,6 @@ const postData = (
       params: string[];
       eventType: string;
       postProcess: {};
-      // async function postData(url = "", data = {}) {
-      //   // Default options are marked with *
-      //   const response = await fetch(url, {
-      //     method: "POST", // *GET, POST, PUT, DELETE, etc.
-      //     mode: "cors", // no-cors, *cors, same-origin
-      //     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      //     credentials: "same-origin", // include, *same-origin, omit
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //       // 'Content-Type': 'application/x-www-form-urlencoded',
-      //     },
-      //     redirect: "follow", // manual, *follow, error
-      //     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      //     body: JSON.stringify(data), // body data type must match "Content-Type" header
-      //   });
-      //   return response.json(); // parses JSON response into native JavaScript objects
-      // }
-      // postData("https://example.com/answer", { answer: 42 }).then((data) => {
-      //   console.log(data); // JSON data parsed by `data.json()` call
-      // });
-      // literally pulling from dev tools running a different flow
     };
   }
 ) => {
