6658412ecbfc1c2cd065987ae741da0fd81e9f38
Anthony Dombrowski <akdflyinvee73@gmail.com>
2022-11-24 21:36:17 -0600

    added button for registering

--- sign-on-page/src/App.tsx
+++ sign-on-page/src/App.tsx
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.css";
+import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
@@ -11,34 +12,39 @@ import Form from "react-bootstrap/Form";
  */
 function App() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="Enter email"
-          autoComplete="username"
-        />
-        <Form.Text className="text-muted">
-          This will also be your username.
-        </Form.Text>
-      </Form.Group>
+    <Container>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            autoComplete="username"
+          />
+          <Form.Text className="text-muted">
+            This will also be your username.
+          </Form.Text>
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Password"
-          autoComplete="new-password"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            autoComplete="new-password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+        <Button variant="primary" type="submit">
+          Create a new account
+        </Button>
+      </Form>
+    </Container>
   );
 }
 
