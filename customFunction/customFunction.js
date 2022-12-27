02c6607bcf5fd515f59ef874dcb1120a5abb99ab
Anthony Dombrowski <akdflyinvee73@gmail.com>
2022-12-27 17:30:45 -0600

    add image links to custom function

--- customFunction/customFunction.js
+++ customFunction/customFunction.js
@@ -1,5 +1,42 @@
 const crypto = require("crypto");
 
+const images = {
+  bg: {
+    seeingDouble: "https://ibb.co/18kJS11",
+    ahhhhhh: "https://ibb.co/tXPr9ks",
+  },
+  me: [
+    "https://ibb.co/Lt7Sv0J",
+    "https://ibb.co/pwqXWjM",
+    "https://ibb.co/w74fDT7",
+    "https://ibb.co/q9W5RTV",
+    "https://ibb.co/Bwr3GDp",
+    "https://ibb.co/KctFsqq",
+    "https://ibb.co/7J9rf8K",
+    "https://ibb.co/K0Jdhtj",
+  ],
+  pointA: [
+    "https://ibb.co/Z2WccwJ",
+    "https://ibb.co/xf3mq32",
+    "https://ibb.co/RNKJJSC",
+    "https://ibb.co/jLyh3kN",
+    "https://ibb.co/kDvrFgT",
+    "https://ibb.co/z6N3YQk",
+    "https://ibb.co/4t4zdyP",
+    "https://ibb.co/NCXsF8L",
+  ],
+  pointB: [
+    "https://ibb.co/HXtGz12",
+    "https://ibb.co/V9gS6Gx",
+    "https://ibb.co/2P35J1y",
+    "https://ibb.co/n0MX1HT",
+    "https://ibb.co/9ZX9WKX",
+    "https://ibb.co/X4HjQB1",
+    "https://ibb.co/vk9cjPN",
+    "https://ibb.co/fN9d8Q9",
+  ],
+};
+
 /**
  * Return a random integer between 0 and the provided max value (max value is
  *  exclusive).
@@ -82,7 +119,12 @@ const noLuckFallback = (
   dvColPosArrayPositions.push(rnd);
   noLuck++;
 
-  return { noLuck, dvColPosSet, dvColVisualizePositionsArray, dvColPosArrayPositions };
+  return {
+    noLuck,
+    dvColPosSet,
+    dvColVisualizePositionsArray,
+    dvColPosArrayPositions,
+  };
 };
 
 const getMinOverlapNeeded = (
@@ -161,7 +203,12 @@ const getPositionWithMinOverlap = (
     iteration++;
   }
 
-  return { rndPosFromLeft, dvColPosSet, dvColVisualizePositionsArray, dvColPosArrayPositions };
+  return {
+    rndPosFromLeft,
+    dvColPosSet,
+    dvColVisualizePositionsArray,
+    dvColPosArrayPositions,
+  };
 };
 
 // doesn't allow direct overlap
@@ -187,7 +234,12 @@ const getPosWithOverlapAllowed = (
     }
   }
 
-  return { rndPosFromLeft, dvColPosSet, dvColVisualizePositionsArray, dvColPosArrayPositions };
+  return {
+    rndPosFromLeft,
+    dvColPosSet,
+    dvColVisualizePositionsArray,
+    dvColPosArrayPositions,
+  };
 };
 
 /**
@@ -217,30 +269,43 @@ const addPosWithAllowableOverlap = (
     );
   }
 
-  ({ rndPosFromLeft, dvColPosSet, dvColVisualizePositionsArray, dvColPosArrayPositions } =
-    getPositionWithMinOverlap(
-      maxIterationsBeforePickingAnyRND,
-      dvColVisualizePositionsArray,
-      dvColPosArrayPositions,
-      dvImgWidth,
-      overlap,
-      rndPosFromLeft,
-      dvColPosSet
-    ));
+  ({
+    rndPosFromLeft,
+    dvColPosSet,
+    dvColVisualizePositionsArray,
+    dvColPosArrayPositions,
+  } = getPositionWithMinOverlap(
+    maxIterationsBeforePickingAnyRND,
+    dvColVisualizePositionsArray,
+    dvColPosArrayPositions,
+    dvImgWidth,
+    overlap,
+    rndPosFromLeft,
+    dvColPosSet
+  ));
 
   if (dvColPosArrayPositions.length === initialPosArrayLength) {
     // ran through without luck, use rndPos anyways
-    ({ noLuck, dvColPosSet, dvColVisualizePositionsArray, dvColPosArrayPositions } =
-      noLuckFallback(
-        dvColVisualizePositionsArray,
-        dvColPosArrayPositions,
-        dvColPosSet,
-        noLuck,
-        dvImgWidth
-      ));
+    ({
+      noLuck,
+      dvColPosSet,
+      dvColVisualizePositionsArray,
+      dvColPosArrayPositions,
+    } = noLuckFallback(
+      dvColVisualizePositionsArray,
+      dvColPosArrayPositions,
+      dvColPosSet,
+      noLuck,
+      dvImgWidth
+    ));
   }
 
-  return { dvColPosSet, dvColVisualizePositionsArray, dvColPosArrayPositions, noLuck };
+  return {
+    dvColPosSet,
+    dvColVisualizePositionsArray,
+    dvColPosArrayPositions,
+    noLuck,
+  };
 };
 
 /**
@@ -265,28 +330,41 @@ const addPosWithOverlap = (
   let rndPosFromLeft = rndPos(dvImgWidth);
 
   // keep iterations to 100 just to avoid infinite loop. at that point we'll probably have to just overlap to avoid too high of an execution time.
-  ({ rndPosFromLeft, dvColPosSet, dvColVisualizePositionsArray, dvColPosArrayPositions } =
-    getPosWithOverlapAllowed(
-      numPosWithoutCompleteOverlap,
-      dvColPosSet,
-      rndPosFromLeft,
-      dvColVisualizePositionsArray,
-      dvColPosArrayPositions
-    ));
+  ({
+    rndPosFromLeft,
+    dvColPosSet,
+    dvColVisualizePositionsArray,
+    dvColPosArrayPositions,
+  } = getPosWithOverlapAllowed(
+    numPosWithoutCompleteOverlap,
+    dvColPosSet,
+    rndPosFromLeft,
+    dvColVisualizePositionsArray,
+    dvColPosArrayPositions
+  ));
 
   if (dvColPosSet.size === initialPosArrayLength) {
     // ran through without luck
-    ({ noLuck, dvColPosSet, dvColVisualizePositionsArray, dvColPosArrayPositions } =
-      noLuckFallback(
-        dvColVisualizePositionsArray,
-        dvColPosArrayPositions,
-        dvColPosSet,
-        noLuck,
-        dvImgWidth
-      ));
+    ({
+      noLuck,
+      dvColPosSet,
+      dvColVisualizePositionsArray,
+      dvColPosArrayPositions,
+    } = noLuckFallback(
+      dvColVisualizePositionsArray,
+      dvColPosArrayPositions,
+      dvColPosSet,
+      noLuck,
+      dvImgWidth
+    ));
   }
 
-  return { dvColPosSet, dvColVisualizePositionsArray, dvColPosArrayPositions, noLuck };
+  return {
+    dvColPosSet,
+    dvColVisualizePositionsArray,
+    dvColPosArrayPositions,
+    noLuck,
+  };
 };
 
 // fill an array while trying to avoid overlap and moving up slowly
@@ -316,42 +394,54 @@ const generateDVColPosArrays = (numOfDVs, dvImgWidth) => {
       positionsOverlapping.withRandom = positionsOverlapping.withRandom + 1;
     } else if (posCreated < maxPositionsWithoutOverlap) {
       // can avoid overlap
-      ({ dvColPosSet, dvColVisualizePositionsArray, dvColPosArrayPositions, noLuck } =
-        addPosWithAllowableOverlap(
-          dvColPosSet,
-          dvColVisualizePositionsArray,
-          dvColPosArrayPositions,
-          dvImgWidth,
-          maxPositionsWithoutOverlap,
-          false,
-          noLuck
-        ));
+      ({
+        dvColPosSet,
+        dvColVisualizePositionsArray,
+        dvColPosArrayPositions,
+        noLuck,
+      } = addPosWithAllowableOverlap(
+        dvColPosSet,
+        dvColVisualizePositionsArray,
+        dvColPosArrayPositions,
+        dvImgWidth,
+        maxPositionsWithoutOverlap,
+        false,
+        noLuck
+      ));
       positionsOverlapping.withoutOverlap =
         positionsOverlapping.withoutOverlap + 1;
     } else if (posCreated < 50) {
       // can allow partial overlap
-      ({ dvColPosSet, dvColVisualizePositionsArray, dvColPosArrayPositions, noLuck } =
-        addPosWithAllowableOverlap(
-          dvColPosSet,
-          dvColVisualizePositionsArray,
-          dvColPosArrayPositions,
-          dvImgWidth,
-          maxPositionsWithoutOverlap,
-          true,
-          noLuck
-        ));
+      ({
+        dvColPosSet,
+        dvColVisualizePositionsArray,
+        dvColPosArrayPositions,
+        noLuck,
+      } = addPosWithAllowableOverlap(
+        dvColPosSet,
+        dvColVisualizePositionsArray,
+        dvColPosArrayPositions,
+        dvImgWidth,
+        maxPositionsWithoutOverlap,
+        true,
+        noLuck
+      ));
       positionsOverlapping.withPartialOverlap =
         positionsOverlapping.withPartialOverlap + 1;
     } else {
       // overlap
-      ({ dvColPosSet, dvColVisualizePositionsArray, dvColPosArrayPositions, noLuck } =
-        addPosWithOverlap(
-          dvColPosSet,
-          dvColVisualizePositionsArray,
-          dvColPosArrayPositions,
-          dvImgWidth,
-          noLuck
-        ));
+      ({
+        dvColPosSet,
+        dvColVisualizePositionsArray,
+        dvColPosArrayPositions,
+        noLuck,
+      } = addPosWithOverlap(
+        dvColPosSet,
+        dvColVisualizePositionsArray,
+        dvColPosArrayPositions,
+        dvImgWidth,
+        noLuck
+      ));
       positionsOverlapping.withOverlap = positionsOverlapping.withOverlap + 1;
     }
 
