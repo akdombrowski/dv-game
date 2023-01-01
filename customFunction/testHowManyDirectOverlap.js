695a13979eda29ec14838df440e33833de3d7f0f
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-01-01 13:04:52 -0600

    added some notes.

--- customFunction/testHowManyDirectOverlap.js
+++ customFunction/testHowManyDirectOverlap.js
@@ -1,11 +1,18 @@
 const a = require("./localRunCustomFunction.js");
 
+/**
+ * Testing so far showing a little under 15% chance of direct overlap
+ */
+
+// 10000 ~ 35s
+// 50000 ~ 3min
 const iterations = 10000;
 let count = 0;
-console.time("totalScriptRuntime")
+console.time("totalScriptRuntime");
 for (let i = 0; i < iterations; i++) {
   const aResults = a();
-  if (aResults.numDirectOverlaps > 0) { // || aResults.noLuck > 0) {
+  if (aResults.numDirectOverlaps > 0) {
+    // || aResults.noLuck > 0) {
     count++;
     // console.log("numDirectOverlaps:", aResults.numDirectOverlaps);
     // console.log("noLuck:", aResults.noLuck);
@@ -13,5 +20,5 @@ for (let i = 0; i < iterations; i++) {
   }
 }
 console.log("count:", count);
-console.log("% with overlap:", count / iterations * 100);
-console.timeEnd("totalScriptRuntime")
+console.log("% with overlap:", (count / iterations) * 100);
+console.timeEnd("totalScriptRuntime");
