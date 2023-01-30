3fcf1dbc788c2be5d5e8aa8e1728e908a7b413b3
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-01-30 04:04:24 -0600

    async versions for timing tests

---
+++ customFunction/testAsyncCustomFunc.js
@@ -0,0 +1,40 @@
+const customFunction = require("./allAsyncLocalRunCustomFunction.js");
+
+/**
+ * Testing so far showing a little under 15% chance of direct overlap
+ */
+
+// old numbers
+// 10000 ~ 35s
+// 50000 ~ 3min
+// 14% chance of direct overlap
+//
+// new numbers
+// 10000 ~ 12s
+// 50000 ~ 1min
+// 4% chance of direct overlap
+//
+// const iterations = 1000;
+// const iterations = 10000;
+// const iterations = 50000;
+const iterations = 1;
+let count = 0;
+console.time("totalScriptRuntime");
+for (let i = 0; i < iterations; i++) {
+  const customFunctionResults = customFunction();
+
+  console.log(JSON.parse(customFunctionResults.renderings));
+  console.log(i);
+
+  if (customFunctionResults.numDirectOverlaps > 0) {
+    // || aResults.noLuck > 0) {
+    count++;
+    // console.log("numDirectOverlaps:", aResults.numDirectOverlaps);
+    // console.log("noLuck:", aResults.noLuck);
+    // console.log("positionsSorted:", aResults.positionsSorted);
+  }
+}
+console.log("runs:", iterations);
+console.log("count of direct overlaps:", count);
+console.log("% with direct overlap:", (count / iterations) * 100);
+console.timeEnd("totalScriptRuntime");
